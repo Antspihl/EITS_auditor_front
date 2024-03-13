cf5134fe1fb0ce6911ddd4f3ddacc8b7767c991e
inpihl <antspihl@gmail.com>
2024-03-13 16:00:10 +0200

    add SYS223M9, SYS223M18 and SYS223M19

--- src/api/MainStore.ts
+++ src/api/MainStore.ts
@@ -1,22 +1,31 @@
 import {defineStore} from "pinia";
 import axios from "axios";
-import {Measure, Sys21M1, Sys21M3, Sys21M6, Sys223M13, Sys223M14, Sys223M5} from "@/molecules/types";
+import * as Sys from "@/molecules/types";
 
 export const API_URL: string = "http://localhost:8080/api/";
 export const useMainStore = defineStore('main', {
   state: () => ({
-    Sys223M5: {} as Sys223M5,
-    Sys21M1: {} as Sys21M1,
-    Sys21M3: {} as Sys21M3,
-    Sys21M6: {} as Sys21M6,
-    Sys223M13: {} as Sys223M13,
-    Sys223M14: {} as Sys223M14,
-    measures: [] as Measure[]
+    Sys21M1: {} as Sys.Sys21M1,
+    Sys21M3: {} as Sys.Sys21M3,
+    Sys21M6: {} as Sys.Sys21M6,
+    Sys223M5: {} as Sys.Sys223M5,
+    Sys223M9: {} as Sys.Sys223M9,
+    Sys223M13: {} as Sys.Sys223M13,
+    Sys223M14: {} as Sys.Sys223M14,
+    Sys223M18: {} as Sys.Sys223M18,
+    Sys223M19: {} as Sys.Sys223M19,
+    measures: [] as Sys.Measure[]
   }),
   getters: {
-    getMeasures(state): Measure[] {
+    getMeasures(state): Sys.Measure[] {
       return state.measures
     },
+    getLoadedMeasures(state): number {
+      return state.measures.length
+    },
+    getAmountOfMeasures(): number {
+      return 9
+    }
   },
   actions: {
     async fetchMeasures() {
@@ -25,28 +34,29 @@ export const useMainStore = defineStore('main', {
       await this.setSys21M1();
       if (Object.keys(this.Sys21M1).length > 0) this.measures.push(this.Sys21M1);
 
-      await this.setSys223M5();
-      if (Object.keys(this.Sys223M5).length > 0) this.measures.push(this.Sys223M5)
-
       await this.setSys21M3();
       if (Object.keys(this.Sys21M3).length > 0) this.measures.push(this.Sys21M3);
 
       await this.setSys21M6();
       if (Object.keys(this.Sys21M6).length > 0) this.measures.push(this.Sys21M6);
 
+      await this.setSys223M5();
+      if (Object.keys(this.Sys223M5).length > 0) this.measures.push(this.Sys223M5);
+
+      await this.setSys223M9();
+      if (Object.keys(this.Sys223M9).length > 0) this.measures.push(this.Sys223M9);
+
       await this.setSys223M13();
       if (Object.keys(this.Sys223M13).length > 0) this.measures.push(this.Sys223M13);
 
       await this.setSys223M14();
       if (Object.keys(this.Sys223M14).length > 0) this.measures.push(this.Sys223M14);
-    },
-    async setSys223M5() {
-      try {
-        const response = await axios.get(API_URL + "SYS223M5", {});
-        this.Sys223M5 = response.data;
-      } catch (error) {
-        console.error("Error fetching setups", error);
-      }
+
+      await this.setSys223M18();
+      if (Object.keys(this.Sys223M18).length > 0) this.measures.push(this.Sys223M18);
+
+      await this.setSys223M19();
+      if (Object.keys(this.Sys223M19).length > 0) this.measures.push(this.Sys223M19);
     },
     async setSys21M1() {
       try {
@@ -72,6 +82,22 @@ export const useMainStore = defineStore('main', {
         console.error("Error fetching setups", error);
       }
     },
+    async setSys223M5() {
+      try {
+        const response = await axios.get(API_URL + "SYS223M5", {});
+        this.Sys223M5 = response.data;
+      } catch (error) {
+        console.error("Error fetching setups", error);
+      }
+    },
+    async setSys223M9() {
+      try {
+        const response = await axios.get(API_URL + "SYS223M9", {});
+        this.Sys223M9 = response.data;
+      } catch (error) {
+        console.error("Error fetching setups", error);
+      }
+    },
     async setSys223M13() {
       try {
         const response = await axios.get(API_URL + "SYS223M13", {});
@@ -87,7 +113,23 @@ export const useMainStore = defineStore('main', {
       } catch (error) {
         console.error("Error fetching setups", error);
       }
-    }
+    },
+    async setSys223M18() {
+      try {
+        const response = await axios.get(API_URL + "SYS223M18", {});
+        this.Sys223M18 = response.data;
+      } catch (error) {
+        console.error("Error fetching setups", error);
+      }
+    },
+    async setSys223M19() {
+      try {
+        const response = await axios.get(API_URL + "SYS223M19", {});
+        this.Sys223M19 = response.data;
+      } catch (error) {
+        console.error("Error fetching setups", error);
+      }
+    },
   }
 })
 
--- src/molecules/types.ts
+++ src/molecules/types.ts
@@ -1,15 +1,5 @@
-export type Measure = {}
-
-export type Sys223M5 = Measure & {
-  firewallEnabled: boolean,
-  antivirusEnabled: boolean,
-  firewallUpToDate: boolean,
-  antivirusUpToDate: boolean,
-}
-
-export type Sys223M13 = Measure & {
-  smartScreenEdgeDisabled: boolean,
-  smartScreenPuaDisabled: boolean
+export type Measure = {
+  [key: string]: any;
 }
 
 export type Sys21M1 = Measure & {
@@ -34,7 +24,30 @@ export type Sys21M6 = Measure & {
   antiMalwareUpToDate: boolean
 }
 
+export type Sys223M5 = Measure & {
+  firewallEnabled: boolean,
+  antivirusEnabled: boolean,
+  firewallUpToDate: boolean,
+  antivirusUpToDate: boolean,
+}
+
+export type Sys223M9 = Measure & {
+  KerberosOrNTLMv2Enabled: boolean
+}
+
+export type Sys223M13 = Measure & {
+  smartScreenEdgeDisabled: boolean,
+  smartScreenPuaDisabled: boolean
+}
+
 export type Sys223M14 = Measure & {
   cortanaDisabled: boolean
 }
 
+export type Sys223M18 = Measure & {
+  allRemoteAssistanceRulesAreAllowed: boolean
+}
+
+export type Sys223M19 = Measure & {
+  allRDPRulesAreAllowed: boolean
+}
