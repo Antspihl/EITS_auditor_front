47ace8e6d3d6687219b616a722f529e5b589336b
inpihl <antspihl@gmail.com>
2024-03-23 11:43:09 +0200

    Added settings for measures

--- src/api/MainStore.ts
+++ src/api/MainStore.ts
@@ -3,12 +3,6 @@ import axios from "axios";
 import * as Sys from "@/molecules/types";
 
 export const API_URL: string = "http://localhost:8080/api";
-// Add measures to this array to fetch them from the API
-export const MEASURES: string[] = [
-  "SYS21M1", "SYS21M3", "SYS21M6",
-  "SYS223M5", "SYS223M9", "SYS223M13",
-  "SYS223M14", "SYS223M18", "SYS223M19"
-];
 
 export const useMainStore = defineStore('main', {
   state: () => ({
@@ -16,6 +10,15 @@ export const useMainStore = defineStore('main', {
       "http://localhost:8080",
       "http://localhost:8081",
       "http://localhost:8082",
+    ] as string[],
+    // Add measures to these arrays to fetch them from the API
+    allMeasures: ["SYS21M1", "SYS21M3", "SYS21M6",
+      "SYS223M5", "SYS223M9", "SYS223M13",
+      "SYS223M14", "SYS223M18", "SYS223M19"
+    ] as string[],
+    selectedMeasures: ["SYS21M1", "SYS21M3", "SYS21M6",
+      "SYS223M5", "SYS223M9", "SYS223M13",
+      "SYS223M14", "SYS223M18", "SYS223M19"
     ] as string[],
     urlLoadingMessages: {} as Record<string, string>,
     loadingArray: [] as string[],
@@ -34,7 +37,7 @@ export const useMainStore = defineStore('main', {
       this.urlMeasures[url] = [];
       this.urlLoadingMessages[url] = `Loading measures for ${url}...`;
       this.loadingArray.push(url);
-      for (const measure of MEASURES) {
+      for (const measure of this.selectedMeasures) {
         this.urlLoadingMessages[url] = `Loading ${measure}...`;
         const response = await this.fetchMeasure(measure, url + "/api");
         if (Object.keys(response).length > 0) this.urlMeasures[url].push(response);
@@ -64,7 +67,7 @@ export const useMainStore = defineStore('main', {
 
     areLoaded(url: string) {
       if (!this.urlMeasures[url]) return false;
-      return this.urlMeasures[url].length === MEASURES.length;
+      return this.urlMeasures[url].length === this.selectedMeasures.length;
     },
 
     getLoadingMessage(url: string) {
@@ -74,7 +77,7 @@ export const useMainStore = defineStore('main', {
 
     getPercentage: function (url: string) {
       if (!this.urlMeasures[url]) return 0;
-      return Math.round(((this.urlMeasures[url].length / MEASURES.length) * 100) * 10) / 10;
+      return Math.round(((this.urlMeasures[url].length / this.selectedMeasures.length) * 100) * 10) / 10;
     },
 
   }
--- src/molecules/MeasureSettings.vue
+++ src/molecules/MeasureSettings.vue
@@ -1,15 +1,54 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <v-expansion-panel>
     <v-expansion-panel-title>
       Meetmed
     </v-expansion-panel-title>
+    <v-expansion-panel-text>
+      <v-row>
+        <v-col>
+          <span>Vali meetmed, mida kontrollida</span>
+          <v-chip-group
+            v-model="selectedMeasures"
+            multiple
+            filter
+          >
+           <v-chip
+              v-for="measure in mainStore.allMeasures"
+              :key="measure"
+              :value="measure"
+           >
+              {{ measure }}
+           </v-chip>
+          </v-chip-group>
+          <v-btn
+            color="primary"
+            @click="saveMeasures"
+            :disabled="selectedMeasures === mainStore.selectedMeasures || selectedMeasures.length === 0"
+          >
+            Salvesta
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-text>
   </v-expansion-panel>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import {useMainStore} from "@/api/MainStore";
+import {onMounted, ref} from "vue";
 
-</style>
+const mainStore = useMainStore();
+const selectedMeasures = ref<string[]>([]);
+const allMeasures = ref<string[]>([]);
+
+function saveMeasures () {
+  if (selectedMeasures.value.length !== 0) {
+    mainStore.selectedMeasures = selectedMeasures.value;
+  }
+}
+
+onMounted(() => {
+  selectedMeasures.value = mainStore.selectedMeasures;
+  allMeasures.value = mainStore.allMeasures;
+});
+</script>
